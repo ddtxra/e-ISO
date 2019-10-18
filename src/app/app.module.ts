77b74e248d94394b48f333cdb21bafa124c3ac59
Daniel Teixeira <ddtxra@gmail.com>
2019-10-18 09:46:45 +0200

    adding comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,27 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { Camera } from '@ionic-native/camera/ngx';//import in app.module.ts
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database";
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+  imports: [BrowserModule, IonicModule.forRoot(), 
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyDxPjj1BFTbRBEn3J6K2zU-8pXPcz0un20",
+      databaseURL: "https://eiso-hug.firebaseio.com",
+      projectId: "eiso-hug",
+      storageBucket: "eiso-hug.appspot.com"
+    }),
+    AngularFireDatabaseModule,
+    AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+     Camera //here
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,49 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public survey: any = {"fever": "Oui"};
 
+  constructor(private camera: Camera, private db: AngularFireDatabase, public navCtrl: NavController) { }
+  private surveyResponsesRef = this.db.list<any>('survey-responses');
+
+  private convertObjectValuesRecursive(obj, target, replacement) {
+    obj = {...obj};
+    Object.keys(obj).forEach((key) => {
+      if (obj[key] == target) {
+        obj[key] = replacement;
+      } else if (typeof obj[key] == 'object' && !Array.isArray(obj[key])) {
+        obj[key] = this.convertObjectValuesRecursive(obj[key], target, replacement);
+      }
+    });
+    return obj;
+  }
+
+  public uploadPicture(event) {
+
+    const options: CameraOptions = {
+        quality: 100,
+        destinationType: this.camera.DestinationType.FILE_URI,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE
+      }
+
+      this.camera.getPicture(options).then((imageData) => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64 (DATA_URL):
+        let base64Image = 'data:image/jpeg;base64,' + imageData;
+      }, (err) => {
+        // Handle error
+      });
+
+  }
+  public sendResponses(event){
+
+    let surveyNoUndefined: Object = this.convertObjectValuesRecursive(this.survey, null, '');
+    surveyNoUndefined["date"] = new Date().toISOString();
+
+    this.surveyResponsesRef.push(surveyNoUndefined)
+      .then( () => this.navCtrl.navigateRoot('/thanks'))
+      .catch(err => alert('Something wrong happened: ' + err));
+     
+  }
 }
